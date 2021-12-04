70319266c3a6b0371b404d96736edb16fe48af44
Lam Huynh <lam.huynh@infodation.vn>
2021-12-04 21:09:31 +0700

    update script

--- generator.sh
+++ generator.sh
@@ -4,7 +4,7 @@ rm -f output.txt blocklist.txt youtubelist.txt
 wget https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt
 wget https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/master/youtubelist.txt
 
-cat output.txt
+#cat output.txt
 filename='blocklist.txt'
 while read line; do
 # reading each line
@@ -16,4 +16,3 @@ while read line; do
 # reading each line
 echo "||$line^" >> output.txt
 done < $filename
-
